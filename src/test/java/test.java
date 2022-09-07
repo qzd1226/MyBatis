449ee11f7add5188ffed3b735e5b098aeb1b9eae
qzd1226 <690160142@qq.com>
2022-09-07 01:37:52 -0500

    以包引入映射文件 要求： mapper接口所在的包要和映射文件所在的包一致 mapper接口要和映射文件的名字一致

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -19,7 +19,7 @@ public class test {
         //通过核心配置文件所对应的字节输入流创建工厂类SqlSessionFactory，生产SqlSession对象
         SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(is);
         //创建SqlSession对象，此时通过SqlSession对象所操作的sql都必须手动提交或回滚事务 //
-        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
         //创建SqlSession对象，此时通过SqlSession对象所操作的sql都会自动提交
         //SqlSession sqlSession = sqlSessionFactory.openSession(true);
         //通过代理模式创建UserMapper接口的代理实现类对象
@@ -36,6 +36,14 @@ public class test {
         list.forEach((customer) -> {
             System.out.println(customer);
         });
+    }
+    @Test
+    public void testUpdate() throws IOException{
+        InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        CustomerMapper mapper = sqlSession.getMapper(CustomerMapper.class);
+        mapper.updateCustomer();
     }
 
 }
